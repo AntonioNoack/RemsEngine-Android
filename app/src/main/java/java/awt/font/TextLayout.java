7b8bb1b01eab4ebdde6c2762f78876d45d6b4218
Antonio Noack <antonio-noack@gmx.de>
2022-02-28 23:40:57 +0100

    cleaned up text mesh generation

--- app/src/main/java/java/awt/font/TextLayout.java
+++ app/src/main/java/java/awt/font/TextLayout.java
@@ -71,6 +71,8 @@ public class TextLayout {
         return (float) bounds.getMaxX();
     }
 
+    // todo why is the shape mirrored?? z space maybe...
+
     public Shape getOutline(AffineTransform transform) {
 
         int w = rect.width();
@@ -92,20 +94,13 @@ public class TextLayout {
         bitmap.getPixels(pixelInts, 0, w, 0, 0, w, h);
         bitmap.recycle();
 
-        System.out.println("Using size " + w + " x " + h + " for " + text);
-
-        if (true) {
-            float[] pixelFloats = new float[w * h];
-            for (int i = 0, l = pixelInts.length; i < l; i++) {
-                pixelFloats[i] = pixelInts[i] >>> 24;
-            }
-            return createMarchingSquaresShape(w, h, pixelFloats);
-        } else {
-            for (int i = 0, l = pixelInts.length; i < l; i++) {
-                pixelInts[i] = (pixelInts[i] >>> 24) - 128;// extract alpha, and make it to [127,-128]
-            }
-            return createPixelatedShape(w, h, dx, dy, pixelInts);
+        // clearly superior compared to the old method
+        // todo we could simplify the shapes, and identify quadratic and cubic splines...
+        float[] pixelFloats = new float[w * h];
+        for (int i = 0, l = pixelInts.length; i < l; i++) {
+            pixelFloats[i] = pixelInts[i] >>> 24;
         }
+        return createMarchingSquaresShape(w, h, pixelFloats);
     }
 
     private static Shape createMarchingSquaresShape(int w, int h, float[] pixelFloats) {
@@ -133,176 +128,6 @@ public class TextLayout {
         shape.points = coordinates;
         shape.operations = operations;
         return shape;
-    }
-
-    private static Shape createPixelatedShape(int w, int h, int dx, int dy, int[] pixelInts) {
-        IntArrayList ops = new IntArrayList(64);
-        FloatArrayList data = new FloatArrayList(256, 0f);
-        IntArrayList edge = new IntArrayList(64);
-        float[] point = new float[2];
-        int ctr = 0;
-        BitSet done = new BitSet(w * (h + 2) * 2);
-        for (int y = 0, i = 0; y < h; y++) {
-            for (int x = 0; x < w - 1; x++, i++) {
-                if (isEdge(x, y, w, pixelInts, true)) {
-
-                    traceEdge(x, y, w, h, pixelInts, done, edge);
-
-                    // todo if the curve is inside, reverse its order (?)
-
-                    if (edge.size() > 2) {
-                        // a full curve was found -> turn into points & lines
-                        edgeToPoint(edge.getValue(0), w, pixelInts, point);
-                        moveTo(point[0] + dx, h - point[1] - 1f, ops, data);
-                        for (int k = 1, l = edge.size(); k < l; k++) {
-                            edgeToPoint(edge.getValue(k), w, pixelInts, point);
-                            lineTo(point[0] + dx, h - point[1] - 1f, ops, data);
-                        }
-                        close(ops);
-                    }
-
-                }
-            }
-            i++;
-        }
-
-        if (ops.isEmpty()) return null;
-        Shape shape = new Shape();
-        shape.points = data.toFloatArray();
-        byte[] ops2 = new byte[ops.size()];
-        for (int i = 0, l = ops2.length; i < l; i++) {
-            ops2[i] = (byte) ops.getValue(i);
-        }
-        shape.operations = ops2;
-        return shape;
-    }
-
-    private static void edgeToPoint(int edge, int w, int[] pixels, float[] dst) {
-        int i = edge >> 1;
-        int x = i % w;
-        int y = i / w;
-        dst[0] = x + 0.5f;
-        dst[1] = y + 0.5f;
-        if (isVEdge(edge)) {
-            dst[0] += cut(pixels[i], pixels[i + 1]);
-        } else {
-            dst[1] += cut(pixels[i], pixels[i + w]);
-        }
-    }
-
-    private static void traceEdge(int x0, int y0, int w, int h, int[] pixels, BitSet done, IntArrayList dst) {
-
-        dst.clear();
-
-        int x = x0;
-        int y = y0;
-        // our direction
-        // 0: ->
-        // 1: V
-        // 2: <-
-        // 3: A
-        int edge;
-        int dir = 1;
-        while (true) {
-
-            int i3 = dir * 3, i;
-            boolean vEdge = (dir & 1) == 1;
-            for (i = 0; i < 3; i++) {
-                int x2 = x + dx[i3];
-                int y2 = y + dy[i3++];
-                if (isEdge(x2, y2, w, pixels, vEdge)) {
-                    // go right
-                    x = x2;
-                    y = y2;
-                    dir = (dir + nextDir[i]) & 3;
-                    break;
-                }
-                vEdge = (dir & 1) == 0;
-            }
-            if (i >= 3) return;
-
-            edge = (x + y * w) * 2 + (dir & 1);
-            dst.plusAssign(edge);
-
-            if (done.get(edge)) return;
-            done.set(edge, true);
-
-        }
-    }
-
-    private static boolean isVEdge(int edge) {
-        return (edge & 1) == 1;
-    }
-
-    private final static int[] dx = new int[]{
-            +1, +0, +0,
-            +0, +0, +1,
-            -1, -1, -1,
-            +0, +1, +0
-    };
-    private final static int[] dy = new int[]{
-            +0, +1, +0,
-            +1, +0, +0,
-            +0, +0, +1,
-            -1, -1, -1
-    };
-    private final static int[] nextDir = new int[]{0, 1, 3};
-
-    private static boolean isEdge(int x, int y, int w, int[] pixels, boolean vEdge) {
-        int i0 = x + y * w, i1;
-        if (i0 < 0 || x >= w || i0 >= pixels.length) {
-            System.out.println(x + "," + y + "is oob");
-            return false;
-        }
-        if (vEdge) {
-            if (x + 1 >= w) return false;
-            i1 = i0 + 1;
-        } else {
-            i1 = i0 + w;
-            if (i1 >= pixels.length) return false;
-        }
-        return (pixels[i0] > 0) != (pixels[i1] > 0);
-    }
-
-    private static float cut(int a, int b) {
-        if ((a < 0) == (b < 0)) return 0.5f;
-        if (a > b) a++;
-        else b++;
-        return a / (float) (a - b);
-    }
-
-    private static void close(IntArrayList ops) {
-        ops.plusAssign(SEG_CLOSE);
-    }
-
-    private static void moveTo(float x, float y, IntArrayList ops, FloatArrayList data) {
-        ops.plusAssign(SEG_MOVETO);
-        data.plusAssign(x);
-        data.plusAssign(y);
-    }
-
-    private static void lineTo(float x, float y, IntArrayList ops, FloatArrayList data) {
-        ops.plusAssign(SEG_LINETO);
-        data.plusAssign(x);
-        data.plusAssign(y);
-    }
-
-    private static void quadTo(float x0, float y0, float x1, float y1, IntArrayList ops, FloatArrayList data) {
-        ops.plusAssign(SEG_QUADTO);
-        data.plusAssign(x0);
-        data.plusAssign(y0);
-        data.plusAssign(x1);
-        data.plusAssign(y1);
-    }
-
-    private static void cubicTo(float x0, float y0, float x1, float y1, float x2, float y2, IntArrayList ops, FloatArrayList data) {
-        ops.plusAssign(SEG_CUBICTO);
-        data.plusAssign(x0);
-        data.plusAssign(y0);
-        data.plusAssign(x1);
-        data.plusAssign(y1);
-        data.plusAssign(x2);
-        data.plusAssign(y2);
     }
 
 }
